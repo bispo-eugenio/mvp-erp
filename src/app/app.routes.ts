f0fc40d2f689a045fb4da6983e401072f46355aa
bispo-eugenio <bispoeugenio270@gmail.com>
2025-09-22 06:15:04 -0300

    create: criação do componente login-page + criação de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './components/login-components/login-page/login-page';
+import { HomePage } from './components/landing-page/home-page/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomePage,
+    },
+    {
+        path: "login",
+        component: LoginPage,
+        title: "Login"
+    }
+];
