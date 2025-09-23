838de7f00ee01eb7eb68656d969fbce7274f60f1
bispo-eugenio <bispoeugenio270@gmail.com>
2025-09-23 06:10:45 -0300

    feat: adiciona template

--- src/app/components/landing-page/home-page/home-page.ts
+++ src/app/components/landing-page/home-page/home-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../base/header/header';
 import { Footer } from '../../base/footer/footer';
 import { InfoService } from '../info-service/info-service';
+import { Contact } from "../contact/contact";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Header, Footer, InfoService],
+  imports: [Header, Footer, InfoService, Contact],
   templateUrl: './home-page.html',
   styleUrl: './home-page.sass'
 })
