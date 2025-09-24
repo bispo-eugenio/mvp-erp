3eaef712e53b76a43557dd576b4c7d00bc7ab176
bispo-eugenio <bispoeugenio270@gmail.com>
2025-09-24 05:52:27 -0300

    feat: adiciona template + estilização da seção about

--- src/app/components/landing-page/home-page/home-page.ts
+++ src/app/components/landing-page/home-page/home-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../base/header/header';
 import { Footer } from '../../base/footer/footer';
 import { InfoService } from '../info-service/info-service';
+import { About } from '../about/about';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Header, Footer, InfoService],
+  imports: [Header, Footer, InfoService, About],
   templateUrl: './home-page.html',
   styleUrl: './home-page.sass'
 })
